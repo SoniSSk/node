8ef44320bb7b618104712f1271b5f4f08ea4f6ce
KuldeepSoniSwapnil <49443497+SoniSSk@users.noreply.github.com>
2023-07-03 10:58:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// const data = require('./data')
+// console.log(data.z())
+// const colors = require('colors')
+// console.log("Hello Kuldeep Soni".rainbow)
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.end("<H1>Hello Kuldeep Soni</H1>")
+// }
+// ).listen(3000)  // 3000 is port number
+
